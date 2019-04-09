559dfe7de7fbf80ecea427c89a311fa9c271329f
seandi <eandi.simo@outlook.it>
2019-04-09 10:45:39 +0200

    Add solution

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+import tododb
 
 app = Flask(__name__)
+task_list = []
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return redirect(url_for('index'))
+
+
+@app.route('/index')
+def index():
+    task_list.clear()
+    for task in tododb.get_tasks():
+        task_list.append(task)
+    return render_template('index.html', task_list=task_list)
+
+
+@app.route('/add_task', methods=['POST', 'GET'])
+def add_task():
+    task = request.form['task_description']
+    tododb.add_task(task)
+    return redirect(url_for('index'))
+
+
+@app.route('/delete_task/<id_task>')
+def delete_task(id_task):
+    print(id_task)
+    tododb.delete_task(id_task)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
